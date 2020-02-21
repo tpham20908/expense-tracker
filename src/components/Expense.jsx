b853b3f5b56459108ef14fd4e306d45b81d7c4f5
Tung Pham <tpham20908@gmail.com>
2020-02-21 15:32:59 -0500

    add PropTypes in Expense

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import formatDate from "../utils/formatDate";
 
@@ -30,6 +31,11 @@ const Expense = ({ expense, deleteExpense }) => {
       </td>
     </tr>
   );
+};
+
+Expense.propTypes = {
+  expense: PropTypes.object,
+  deleteExpense: PropTypes.func.isRequired
 };
 
 export default Expense;
